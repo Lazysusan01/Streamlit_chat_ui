aec805b78b5af5de274e126c34a986b5a3487fea
Nicomcgill <nicomcgill@gmail.com>
2024-05-16 15:37:22 +1000

    Adding more SDS to the demo

--- app.py
+++ app.py
@@ -41,12 +41,14 @@ def ask_claude(system_prompt,messages):
 
 def main():
     st.set_page_config(page_title="Chemwatch SDS Chatbot", page_icon=":robot_face:")
+    st.title("Chemwatch SDS Chatbot :robot_face:")
+    # Chemwatch logo in sidebar
     sidebar_image = Image.open("CW_logo.png")
     st.sidebar.image(sidebar_image)
-    st.title("Chemwatch SDS Chatbot :robot_face:")
-    
+    # Select Language and chemicals
     language = st.sidebar.selectbox("Select a language", ["English", "French", "Spanish", "Uwu","Pirate"])
     st.sidebar.title("Select a chemical or select two to compare:")
+    # Styling the sidebar
     sidebar_styles = """
     /* Sidebar styles */
     [data-testid="stSidebar"] > div {
@@ -59,7 +61,6 @@ def main():
         align: center;
     }
     """
-    
     st.markdown(f"<style>{sidebar_styles}</style>", unsafe_allow_html=True)
     
     # List all the files in the files directory
@@ -91,17 +92,33 @@ def main():
                 st.write(message["content"])
             
         if len(selected_chemical) == 1:
+            
+            suggested_queries = ['What is the chemical formula of this chemical?', 
+                                'What are the hazards of this chemical?',
+                                'What are the first aid measures for this chemical?',
+                                'What are the physical properties of this chemical?', 
+                                'What are the storage and handling instructions for this chemical?']
+            
             context = chemical_content[selected_chemical[0]]
             system_prompt = f"This is an SDS for {selected_chemical}, you will be asked questions about it, please do not answer any questions outside of the sds, please keep your answer brief but not truncated, respond only in {language}. <SDS>{context}</SDS>"
             query = st.chat_input(f"Ask me anything about {selected_chemical[0]}")
             # st.session_state.messages.append({"role": "assistant", "content": f"Ask me anything about {selected_chemical}"})
         elif len(selected_chemical) == 2:
+            suggested_queries = ['What are the differences between these two chemicals? (format your answer as a table)',
+                                 'What are the first aid measures for these two chemicals?',
+                                 'What are the physical properties of these two chemicals?',
+                                 'What are the storage and handling instructions for these two chemicals?']
+
             chemical1 = chemical_content[selected_chemical[0]]
             chemical2 = chemical_content[selected_chemical[1]]
             system_prompt = f"These are SDS for {selected_chemical[0]} and {selected_chemical[1]}, you will be asked questions about them, please do not answer any questions outside of the sds, please keep your answer brief but not truncated, respond only in {language}. <SDS1>{chemical1}</SDS1> <SDS2>{chemical2}</SDS2>"
             query = st.chat_input(f"Ask me anything about {selected_chemical[0]} and {selected_chemical[1]}")
             # st.session_state.messages.append({"role": "assistant", "content": f"Ask me anything about {selected_chemical[0]} and {selected_chemical[1]}"})
-        
+        query_placeholder = st.empty()
+        for question in suggested_queries: 
+            if st.button(question):
+                query = question
+                
         if query:
             st.session_state.messages.append({"role": "user", "content": query})
             with st.chat_message("user"):
